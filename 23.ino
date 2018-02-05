66703f13558072a1834b8da72e812470632a10ca
EsZe <33262870+eszepto@users.noreply.github.com>
2018-02-05 22:42:05 +0700

    Create 23.ino

---
+++ 23.ino
@@ -0,0 +1,33 @@
+int doubledotpause()   //11 as SW to pause
+{
+    for(i=3;i<=8;i++)// pin3-8 as LED
+    {
+    digitalWrite(i,HIGH);
+    digitalWrite(i+1,HIGH);
+    Serial.println(i);
+    for(int n=9;n>=3;n--) 
+    {
+        if(n != i && n != i+1)
+        {
+            digitalWrite(n,LOW);
+        }
+    }
+
+    if(digitalRead(11) == LOW) 
+    {   
+     delay(10); 
+     while(digitalRead(11) == LOW){} 
+     while(true)
+     {
+       if(digitalRead(11) == LOW) 
+        {
+            while(digitalRead(11) == LOW){}
+            break;
+        }
+                
+     }
+    }
+    delay(300);
+    
+    }
+}
