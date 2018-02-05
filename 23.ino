4e61e1a1d1e008321f42c935f75439fe3eade17f
EsZe <33262870+eszepto@users.noreply.github.com>
2018-02-05 23:27:21 +0700

    Update 23.ino

--- 23.ino
+++ 23.ino
@@ -1,7 +1,7 @@
+int i=3;
 int doubledotpause()   //11 as SW to pause
 {
-    for(i=3;i<=8;i++)// pin3-8 as LED
-    {
+    
     digitalWrite(i,HIGH);
     digitalWrite(i+1,HIGH);
     Serial.println(i);
@@ -14,21 +14,13 @@ int doubledotpause()   //11 as SW to pause
     }
 
     if(digitalRead(11) == LOW && digitalRead(12) == LOW)  //
-    {   
-     delay(10); 
-     while(digitalRead(11) == LOW || digitalRead(12) == LOW){} //
-     while(true)
-     {
-       if(digitalRead(11) == LOW && digitalRead(12) == LOW)  //
-        {
-            while(digitalRead(11) == LOW || digitalRead(12) == LOW){} //
-            break;
-        }
-                
-     }
-    }
+      {   
+       delay(10); 
+       while(digitalRead(11) == LOW || digitalRead(12) == LOW){} //
+       i++;
+      }
     delay(300);
-    
+  
     }
 }
 
